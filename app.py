a4398e7794a6adca49aa1da0725c12887b972e11
guszx7 <gustavoorodsoares@gmail.com>
2024-11-14 13:21:40 -0300

    Changes by gbriel coêlho

--- app.py
+++ app.py
@@ -3,33 +3,12 @@ from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
-def inicial():
-    return render_template('template.html')
+def home():
+    return render_template('base.html')
 
 @app.route('/exemplo')
 def exemplo():
     return render_template('exemplo.html')
-
-@app.route('/exercicio')
-def exercicio():
-    return render_template('exercicio.html')
-
-@app.route('/exercicio_1')
-def exercicio_1():
-    return render_template('exercicio_1.html')
-
-@app.route('/exercicio_2')
-def exercicio_2():
-    return render_template('exercicio_2.html')
-
-@app.route('/exercicio_3')
-def exercicio_3():
-    return render_template('exercicio_3.html')
-
-@app.route('/exercicios_4')
-def exercicio_4():
-    return render_template('exercicio_4.html')
-
 
 
 if __name__ == '__main__':
--- populate_db.py
+++ populate_db.py
@@ -18,34 +18,47 @@ def create_fake_funcionarios(num):
         )
         funcionario.save()
 
-def create_fake_categorias(num):
+def create_fake_categorias():
     categorias = [
         'Eletrônicos', 'Alimentos', 'Roupas', 'Móveis', 'Livros',
         'Ferramentas', 'Brinquedos', 'Cosméticos', 'Esporte',
         'Jardinagem', 'Veículos', 'Informática', 'Bebidas',
         'Utensílios Domésticos', 'Material Escolar'
     ]
-    for i in range(num):
-        categoria = Categoria(
-            nome_categoria=categorias[i % len(categorias)]  # Cicla pelas categorias realistas
-        )
+    for nome_categoria in categorias:
+        categoria = Categoria(nome_categoria=nome_categoria)
         categoria.save()
 
-def create_fake_produtos(num, categoria_ids):
-    produtos = [
-        'Smartphone', 'Notebook', 'Geladeira', 'Televisão', 'Bicicleta',
-        'Sofá', 'Livro', 'Cafeteira', 'Fogão', 'Fone de Ouvido',
-        'Mouse', 'Teclado', 'Cerveja', 'Refrigerante', 'Camiseta',
-        'Chave Inglesa', 'Boneca', 'Brinquedo de Montar', 'Shampoo',
-        'Bola de Futebol', 'Tesoura', 'Regador', 'Carro', 'Motocicleta'
-    ]
-    for _ in range(num):
-        produto = Produto(
-            nome_produto=random.choice(produtos),
-            preco_produto=round(random.uniform(10.0, 2000.0), 2),
-            id_categoria=random.choice(categoria_ids)
-        )
-        produto.save()
+def create_fake_produtos():
+    categoria_produto_map = {
+        'Eletrônicos': [('Smartphone', 500, 3000), ('Notebook', 1000, 5000), ('Televisão', 800, 4000)],
+        'Alimentos': [('Cerveja', 5, 20), ('Refrigerante', 3, 10)],
+        'Roupas': [('Camiseta', 20, 100)],
+        'Móveis': [('Sofá', 300, 2000), ('Geladeira', 1000, 3000), ('Fogão', 300, 1500)],
+        'Livros': [('Livro', 20, 150)],
+        'Ferramentas': [('Chave Inglesa', 10, 50)],
+        'Brinquedos': [('Boneca', 20, 150), ('Brinquedo de Montar', 30, 200)],
+        'Cosméticos': [('Shampoo', 10, 50)],
+        'Esporte': [('Bola de Futebol', 50, 200)],
+        'Jardinagem': [('Regador', 15, 100)],
+        'Veículos': [('Carro', 15000, 50000), ('Motocicleta', 5000, 20000)],
+        'Informática': [('Mouse', 20, 150), ('Teclado', 50, 300)],
+        'Bebidas': [('Cerveja', 5, 15), ('Refrigerante', 3, 8)],
+        'Utensílios Domésticos': [('Cafeteira', 50, 300)],
+        'Material Escolar': [('Tesoura', 5, 20)]
+    }
+
+    categorias = db_session.query(Categoria).all()
+    categoria_ids = {categoria.nome_categoria: categoria.id_categoria for categoria in categorias}
+
+    for categoria, produtos in categoria_produto_map.items():
+        for nome_produto, preco_min, preco_max in produtos:
+            produto = Produto(
+                nome_produto=nome_produto,
+                preco_produto=round(random.uniform(preco_min, preco_max), 2),
+                id_categoria=categoria_ids[categoria]
+            )
+            produto.save()
 
 def create_fake_movimentacoes(num, funcionario_ids, produto_ids):
     for _ in range(num):
@@ -68,11 +81,11 @@ def main():
     funcionarios = db_session.query(Funcionario).all()
     funcionario_ids = [f.id_funcionario for f in funcionarios]
 
-    create_fake_categorias(10)
+    create_fake_categorias()
     categorias = db_session.query(Categoria).all()
     categoria_ids = [c.id_categoria for c in categorias]
 
-    create_fake_produtos(100, categoria_ids)
+    create_fake_produtos()
     produtos = db_session.query(Produto).all()
     produto_ids = [p.id_produto for p in produtos]
 
